7ee050993d47016e3418b3b7fe4b758a4a1fcba3
Sebastian Fuchs <12152164+foxable@users.noreply.github.com>
2018-11-01 10:36:16 +0100

    Migrate to gatsby v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `foxable.net`,
+    title: 'Gatsby Default Starter',
   },
   plugins: [
-    `gatsby-plugin-react-next`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-sass`
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }
